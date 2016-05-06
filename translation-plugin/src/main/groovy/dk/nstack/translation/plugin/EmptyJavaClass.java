03598c113c609747e82996316a70eaa2135809ac
Johnny Sørensen <joso@nodes.dk>
2016-05-06 18:15:05 +0200

    Added empty java class so we make some javadoc for sonatype to be happy

---
+++ translation-plugin/src/main/groovy/dk/nstack/translation/plugin/EmptyJavaClass.java
@@ -0,0 +1,17 @@
+package dk.nstack.translation.plugin;
+
+/**
+ * Created by joso on 06/05/16.
+ */
+public class EmptyJavaClass {
+
+    /**
+     * Workaround class file for gradle bug:
+     * https://issues.gradle.org/browse/GRADLE-3169 or https://issues.sonatype.org/browse/OSSRH-637
+     * This is a groovy project only
+     */
+    public void testFunction() {
+
+    }
+
+}
