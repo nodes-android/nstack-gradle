834e0561979d7fc7e4d3b6b3dfb833c64af70d53
lucassales <saless182@gmail.com>
2019-08-07 11:25:31 +0200

    Fix to latest NStack build

--- translation-plugin/src/main/groovy/dk/nstack/translation/plugin/AssetManager.groovy
+++ translation-plugin/src/main/groovy/dk/nstack/translation/plugin/AssetManager.groovy
@@ -2,7 +2,6 @@ package dk.nstack.translation.plugin
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
-import groovy.json.internal.LazyMap
 
 class AssetManager {
 
@@ -42,32 +41,32 @@ class AssetManager {
         }
     }
 
-    private static LazyMap getTranslationsFrom(String url) {
+    private static Map getTranslationsFrom(String url) {
         String jsonString = Util.getTextFromUrl(url)
         if (jsonString.isEmpty()) {
-            return new LazyMap()
+            return new HashMap()
         }
         return new JsonSlurper().parseText(jsonString).data
     }
 
-    static LazyMap saveAllTranslationsToAssets() {
+    static Map saveAllTranslationsToAssets() {
         checkIfAssetsFolderExists()
 
         String url = TranslationPlugin.project.translation.contentUrl + "api/v2/content/localize/resources/platforms/mobile"
         String indexJson = Util.getTextFromUrl(url)
         if (indexJson.isEmpty()) {
-            return new LazyMap()
+            return new HashMap()
         }
 
         removePreviousTranslations()
 
-        LazyMap allTranslations = new LazyMap()
+        HashMap allTranslations = new HashMap()
 
-        ArrayList indexResults = new JsonSlurper().parseText(indexJson)
+        ArrayList indexResults = new JsonSlurper().parseText(indexJson).data
         indexResults.eachWithIndex { result, index ->
             String locale = result.language.locale
             File path = getTranslationsPath(index, locale)
-            LazyMap translations = getTranslationsFrom(result.url)
+            HashMap translations = getTranslationsFrom(result.url)
             path.text = JsonOutput.toJson(translations)
             allTranslations[locale] = translations
         }
